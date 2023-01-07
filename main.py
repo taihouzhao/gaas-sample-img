7b368a09291fe5e2f7994c35c06bfc6069e30237
xingzhong <xingzhong@me.com>
2023-01-07 22:03:07 +0800

    add actual files

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from diffusers import StableDiffusionPipeline, DDIMScheduler
 import torch
 from tqdm.auto import tqdm
-
+import hashlib
 
 def promptMagic(category):
     return {
-        "prompt": f"a photo of {category}, looking at camera, extra details, 4k, realistic photo",
+        "prompt": f"a portrait photo of {category}, looking at camera, extra details, 4k, realistic photo",
         "negative_prompt": "ugly face, blurry, bad teeth, big teeth, dark skin, overweight, strong beard, old, fat, closed eye, missing, Poorly drawn face, Morbid, half face",
-        "num_inference_steps": 100,
-        "guidance_scale": 7.5,
+        "num_inference_steps": 50,
+        "guidance_scale": 8,
         "height": 512,
         "width": 512,
         "num_images_per_prompt": 1,
@@ -20,10 +20,11 @@ def inference(model_name, category):
     pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
     pipe.set_progress_bar_config(disable=True)
     prompt = promptMagic(category)
-    num = 4
+    num = 256
     for idx in tqdm(range(num), desc="Generate images", total=num):
         img = pipe(**prompt).images[0]
-        temp_name = f"/img/{category}/{idx}.png"
+        hash_image = hashlib.sha1(img.tobytes()).hexdigest()
+        temp_name = f"/img/{category}/{idx}-{hash_image}.png"
         img.save(temp_name)
 
 if __name__ == "__main__":
--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 set -euxo pipefail
 
 docker build -t gaas-sample-img .
-docker run --gpu all --rm -v $(pwd)/img:/img -v /opt/local/HF:/HF gaas-sample-img
+docker run --gpus all --rm -v $(pwd)/img:/img -v /opt/local/HF:/HF gaas-sample-img
+tar -zcvf img.tar.gz img/
